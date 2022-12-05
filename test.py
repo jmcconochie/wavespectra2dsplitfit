corrected units in test.py and error in wavespec

test.py
@@ -6,6 +6,7 @@ def readspec_mat(filename, dates="td", freq="fd", dirn="thetad", spec2d="spec2d"
     #   fd[nFre] - vector of wave frequencies in Hz
     #   thetad[nDir] - vector of wave directions in degrees
     #   spec2d[nTimes,nFre,nDir] - array of 2D wave spectra for each time in m^2/(Hz.deg)
+    import numpy as np
     
     import scipy.io
     mat = scipy.io.loadmat(filename)
@@ -13,9 +14,8 @@ def readspec_mat(filename, dates="td", freq="fd", dirn="thetad", spec2d="spec2d"
     tm = mat[dates]
     f = mat[freq]
     th = mat[dirn]
-    S = mat[spec2d]
+    S = mat[spec2d] * np.pi/180
 
-    import numpy as np
     import datetime as dt
     sDate = [dt.datetime(x[0],x[1],x[2],x[3],x[4],x[5]) for x in tm]
 

wavespectra2dsplitfit/wavespec.py
@@ -1226,7 +1226,7 @@ class waveSpec:
             ta.pcolormesh(self.f,self.th,np.log(np.transpose(S+1e-9)),clim=[-15,0]) 
             if fConfig['useWind']: ta.contour(wsSpec.f,wsSpec.th,np.transpose(wsMask),levels=[0.5],colors='white')
             plotPeaks(ta)
-            if 'fTime' in keys(fConfig):
+            if 'fTime' in fConfig.keys():
                 ta.set_title(f"Input@{fConfig['fTime']},{fConfig['iTime']}")
             else:
                 ta.set_title(fConfig('tag'))    
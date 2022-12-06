d845dd39021880e58f65c59f056ce6f19080a20c
Jason McConochie <jason.mcconochie@gmail.com>
2022-12-07 07:21:56 +0800

    Added matlab main.m to show usage with matlab (macos)

---
+++ main.m
@@ -0,0 +1,17 @@
+% Example mat file shows how the matlab file should be created
+% for use with fitFromMatlabFile.py
+%
+% Variable required in mat file:
+%   td[nTimes,6] - matrix of date vector each row contains year, month, day, hour, minute, second 
+%   fd[nFre] - vector of spectral frequencies in Hz
+%   thetad[nDir] - vector of spectral directions in deg
+%   spec2d[nTimes, nFre, nDir] - matrix of spectral densities S(f,th) in
+%   m^2/(Hz.deg)
+% 
+% 
+
+load('data/ExampleWaveSpectraObservations.mat')
+whos
+unix('pip3 install wavespectra2dsplitfit')
+unix('python3 fitFromMatlabFile.py data/ExampleWaveSpectraObservations.mat')
+res = readtable('fittedParms.csv')

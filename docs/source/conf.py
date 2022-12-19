d56f815c99a56fe7c42d1b69fad53a8944b57a12
Jason McConochie <jason.mcconochie@gmail.com>
2022-12-20 08:33:09 +1000

    Added starting documentation for MakeTheDocs

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'WaveSpectra2DSplitFit'
+copyright = '2022, Jason McConochie'
+author = 'Jason McConochie'
+release = '0.4.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+.. WaveSpectra2DSplitFit documentation master file, created by
+   sphinx-quickstart on Tue Dec 20 08:10:10 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to WaveSpectra2DSplitFit's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+|flowchart|
+
+.. |flowchart| image:: _static/wavespectra2dsplitfit_algorithm_flowchart.png
+                :scale: 20 %

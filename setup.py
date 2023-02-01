d1bd5384e59998c340bd2da9f8170d3ce20d36ed
Jason McConochie <jason.mcconochie@gmail.com>
2023-02-01 09:06:01 +0000

    Rev up

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 setuptools.setup(
         name="wavespectra2dsplitfit",
-        version="0.5.4",
-        author="Jason McConochie"
+        version="0.5.5",
+        author="Jason McConochie",
         description="Ocean wave 2D spectrum partitioning and fitting JONSWAP spectrum",
         packages=["wavespectra2dsplitfit"]
         )

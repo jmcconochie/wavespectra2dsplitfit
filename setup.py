3426ea46b7f76e04911104d154414f5e177cee0b
Jason McConochie <jason.mcconochie@gmail.com>
2023-02-01 09:02:28 +0000

    Added setup.py to try to get conda pypi pack. work

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+        name="wavespectra2dsplitfit",
+        version="0.5.4",
+        author="Jason McConochie"
+        description="Ocean wave 2D spectrum partitioning and fitting JONSWAP spectrum",
+        packages=["wavespectra2dsplitfit"]
+        )
